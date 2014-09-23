dfa3b123df31d29aaec430516597049f4acdee4f
Mackenzie Child <mackenzie.child@gmail.com>
2014-09-23 12:16:53 -0600

    Add basic authentication to pins with devise

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
@@ -9,11 +10,11 @@ class PinsController < ApplicationController
 	end
 
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 
 		if @pin.save
 			redirect_to @pin, notice: "Pin was successfully created"
