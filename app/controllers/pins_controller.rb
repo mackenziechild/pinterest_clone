c90dd347e50df3e37a1cca3c82992e3f8fe065f8
Mackenzie Child <mackenzie.child@gmail.com>
2014-09-23 11:32:57 -0600

    Pins CRUD abilities

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,51 @@
 class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@pins = Pin.all.order("created_at DESC")
 	end
+
+	def show
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+
+		if @pin.save
+			redirect_to @pin, notice: "Pin was successfully created"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: "Pin was successfully updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+
 end
